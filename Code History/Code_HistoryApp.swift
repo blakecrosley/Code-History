2cda05d10d08ce6b346d8314b09de89de69d6193
blakecrosley <blake.crosley@gmail.com>
2024-06-12 10:04:40 -0700

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Blake Crosley on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
